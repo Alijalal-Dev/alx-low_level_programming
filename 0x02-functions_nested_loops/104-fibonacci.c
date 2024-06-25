086427a457b40f556e7a88ce224c2c3ffad5a7ac
Alijalal-Dev <aj0546006@gmail.com>
2024-06-25 15:38:40 -0700

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,47 +1,58 @@
 #include <stdio.h>
 
 /**
+ * nbrLength - returns the number of digits in a number
+ * @nbr: the number to get its length
+ *
+ * Return: number of digits
+ */
+int nbrLength(unsigned long nbr)
+{
+	int length = 0;
+
+	if (nbr == 0)
+		return (1);
+
+	while (nbr != 0)
+	{
+		nbr /= 10;
+		length++;
+	}
+	return (length);
+}
+
+/**
  * main - Entry point, prints the first 98 Fibonacci numbers
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i;
-	unsigned long a = 1, b = 2, next;
-	unsigned long a_half1, a_half2, b_half1, b_half2;
-	unsigned long half1, half2;
+	int count, initial_zero;
+	unsigned long first = 1, second = 2, next, max = 100000000;
+	unsigned long first_part = 0, second_part = 0, next_part = 0;
 
-	printf("%lu, %lu", a, b);
-
-	for (i = 2; i < 98; i++)
+	for (count = 1; count <= 98; count++)
 	{
-		if (b > 100000000)
+		if (first_part > 0)
+			printf("%lu", first_part);
+		initial_zero = nbrLength(max) - 1 - nbrLength(first);
+		while (first_part > 0 && initial_zero > 0)
 		{
-			a_half1 = a / 100000000;
-			a_half2 = a % 100000000;
-			b_half1 = b / 100000000;
-			b_half2 = b % 100000000;
-			half1 = a_half1 + b_half1;
-			half2 = a_half2 + b_half2;
-			if (half2 > 100000000)
-			{
-				half1 += 1;
-				half2 %= 100000000;
-			}
-			printf(", %lu%010lu", half1, half2);
-			a = b;
-			b = half1 * 100000000 + half2;
+			printf("%d", 0);
+			initial_zero--;
 		}
+		printf("%lu", first);
+		next = (first + second) % max;
+		next_part = first_part + second_part + (first + second) / max;
+		first = second;
+		first_part = second_part;
+		second = next;
+		second_part = next_part;
+		if (count != 98)
+			printf(", ");
 		else
-		{
-			next = a + b;
-			printf(", %lu", next);
-			a = b;
-			b = next;
-		}
+			printf("\n");
 	}
-
-	printf("\n");
 	return (0);
 }
