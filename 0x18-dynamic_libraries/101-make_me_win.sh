fe33722058cd33ef8143a362b4f061c1ba7d3d4d
Alijalal-Dev <aj0546006@gmail.com>
2024-09-23 05:22:58 -0700

    task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/libgiga.so https://github.com/Alijalal-Dev/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+wget -P /tmp https://github.com/Alijalal-Dev/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
